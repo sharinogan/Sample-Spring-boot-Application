90f2665f2bd27d4ba7236868366ef150bdabb13a
Voragun Supawong <voragun2543@gmail.com>
2022-08-15 20:31:03 +0700

    set server port and try to get result from /test

--- code/main/Start.java
+++ code/main/Start.java
@@ -4,42 +4,26 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 class Start {
 
     public static void main(String[] data) {
         ApplicationContext context;
         context = SpringApplication.run(Setup.class);
-        Cryptocurrency x = context.getBean("bitcoin",Cryptocurrency.class);
-        System.out.println(x.symbol);
-        System.out.println(x.rate);
-        SpringApplication.exit(context);
+        
+//        SpringApplication.exit(context);
     }
 }
 
 @SpringBootApplication
-class Setup {
+class Setup { }
 
-    @Bean("ethereum")
-    Cryptocurrency create() {
-        Cryptocurrency c = new Cryptocurrency();
-        c.symbol = "ETH/USD";
-        c.rate = 1925.00;
-        return c;
+@RestController
+class Sample {
+    @RequestMapping("/test")
+    double showTest() {
+        return 123.4567;
     }
-
-    // Create another bean, with BTC/USD
-    @Bean("bitcoin")
-    Cryptocurrency bitcoin() {
-        Cryptocurrency c = new Cryptocurrency();
-        c.symbol = "ฺBTC/USD";
-        c.rate = 2000.00;
-        return c;
-    }
-}
-
-class Cryptocurrency {
-
-    String symbol;
-    double rate;
 }
